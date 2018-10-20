45aeb498bb3ee5f25e5c324e6cad2dac8e0406bc
kevinleptin <kevinleptin@gmail.com>
2018-10-20 16:33:19 +0800

    add auto refresh for MP access tokens

--- JinZhou.V2/Controllers/api/TokenController.cs
+++ JinZhou.V2/Controllers/api/TokenController.cs
@@ -7,11 +7,21 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using JinZhou.V2.Handler;
+using JinZhou.V2.Models;
+using JinZhou.V2.Services;
+using Senparc.Weixin.Open.ComponentAPIs;
 
 namespace JinZhou.V2.Controllers.api
 {
     public class TokenController : ApiController
     {
+        private ApplicationDbContext _context;
+
+        public TokenController()
+        {
+            _context = ApplicationDbContext.Create();
+        }
+
         [HttpPost, Route("MPAccount/Auth")]
         public IHttpActionResult RefreshVerifyToken()
         {
@@ -27,8 +37,30 @@ namespace JinZhou.V2.Controllers.api
             var Request = System.Web.HttpContext.Current.Request;
             WxServerNoticeHandler handler = new WxServerNoticeHandler(Request.InputStream, pm);
             handler.Execute();
-            
+            RefreshMpAccessCode();
+
             return Ok("success");
+        }
+
+        private void RefreshMpAccessCode()
+        {
+            var componentToken = ComponentTokenService.GetInstance().Token;
+            string componentAppId = ConfigurationManager.AppSettings["AppId"];
+            var mpTokenList = _context.MpTokens.ToList();
+            foreach (var mpToken in mpTokenList)
+            {
+                if (DateTime.Now.AddSeconds(600) >= mpToken.RefreshOn.AddSeconds(mpToken.ExpiredIn))
+                {
+                  var refreshRlt =  ComponentApi.ApiAuthorizerToken(componentToken.ComponentAccessToken, componentAppId,
+                        mpToken.MpAppId, mpToken.MpRefreshToken);
+                    mpToken.ExpiredIn = refreshRlt.expires_in;
+                    mpToken.MpAccessToken = refreshRlt.authorizer_access_token;
+                    mpToken.MpRefreshToken = refreshRlt.authorizer_refresh_token;
+                    mpToken.RefreshOn = DateTime.Now;
+                    _context.SaveChanges();
+                }
+            }
+
         }
     }
 }
